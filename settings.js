f274e76ea4f6ac098482c580ef4339513dbf837b
erasmkuu-droid <erasmkuu@gmail.com>
2025-08-31 12:37:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;CI82HCwC#l14GFGl8aNowP6ttB2LvFdrsGg_x73wNt4R6adg8_vo", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254795443901", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
